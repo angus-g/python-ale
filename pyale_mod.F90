9c8f08060e118d660c3e5bfc2c04988dd04c0e9f
Angus Gibson <angus@agibson.me>
2022-06-16 15:14:00 +1000

    Wrap up all static initialisation into one object
    Hopefully we can pass this object back/forth into Python land with our
    model state.

--- pyale_mod.F90
+++ pyale_mod.F90
@@ -8,29 +8,37 @@ module pyale_mod
   use MOM_open_boundary, only : ocean_OBC_type
   use MOM_unit_scaling, only : unit_no_scaling_init, unit_scale_type
   use MOM_transcribe_grid, only : copy_dyngrid_to_MOM_grid
+  use MOM_verticalGrid, only : verticalGridInit, verticalGrid_type
 
   implicit none ; private
 
   public :: create_domain
-contains
 
-  subroutine create_domain
+  type, public :: MOM_state_type ; private
     type(ocean_grid_type) :: G
     type(param_file_type) :: param_file
     type(hor_index_type) :: HI
     type(dyn_horgrid_type), pointer :: dG => NULL()
     type(unit_scale_type), pointer :: US => NULL()
     type(ocean_OBC_type), pointer :: OBC => NULL()
+    type(verticalGrid_type), pointer :: GV => NULL()
+  end type MOM_state_type
 
-    call open_param_file("MOM_input", param_file)
-    call MOM_domains_init(G%domain, param_file, symmetric=.true., domain_name="MOM_in")
-    call hor_index_init(G%domain, HI, param_file)
-    call create_dyn_horgrid(dG, HI)
-    call clone_MOM_domain(G%domain, dG%domain)
-    call unit_no_scaling_init(US)
-    call MOM_grid_init(G, param_file, US=US, HI=HI)
-    call MOM_initialize_fixed(dG, US, OBC, param_file, write_geom=.false., output_dir=".")
-    call copy_dyngrid_to_MOM_grid(dG, G, US)
+contains
+
+  subroutine create_domain
+    type(MOM_state_type) :: CS
+
+    call open_param_file("MOM_input", CS%param_file)
+    call MOM_domains_init(CS%G%domain, CS%param_file, symmetric=.true., domain_name="MOM_in")
+    call hor_index_init(CS%G%domain, CS%HI, CS%param_file)
+    call create_dyn_horgrid(CS%dG, CS%HI)
+    call clone_MOM_domain(CS%G%domain, CS%dG%domain)
+    call unit_no_scaling_init(CS%US)
+    call MOM_grid_init(CS%G, CS%param_file, US=CS%US, HI=CS%HI)
+    call MOM_initialize_fixed(CS%dG, CS%US, CS%OBC, CS%param_file, write_geom=.false., output_dir=".")
+    call copy_dyngrid_to_MOM_grid(CS%dG, CS%G, CS%US)
+    call verticalGridInit(CS%param_file, CS%GV, CS%US)
 
     print *, "create_domain done"
   end subroutine create_domain
