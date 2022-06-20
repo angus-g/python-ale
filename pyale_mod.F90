dd55197f15a611eb11959edd7d357e62157e9a19
Angus Gibson <angus@agibson.me>
2022-06-20 16:33:59 +1000

    Convert to f90wrap to allow proper derived type support
    We also do a bit of fiddling to get the required dimensions for output
    arrays out, because we have to allocate them on the Python
    side. Because we compile with -fdefault-real-8, this means
    manipulating the type map.

--- pyale_mod.F90
+++ pyale_mod.F90
@@ -1,5 +1,6 @@
 module pyale_mod
   use, intrinsic :: iso_c_binding
+  use, intrinsic :: iso_fortran_env
 
   use MOM_domains, only : MOM_domains_init, MOM_domain_type, clone_MOM_domain
   use MOM_dyn_horgrid, only : create_dyn_horgrid, dyn_horgrid_type
@@ -17,9 +18,9 @@ module pyale_mod
 
   implicit none ; private
 
-  public :: init_MOM_state, load_MOM_restart, init_MOM_ALE, destroy_MOM_state
+  public :: init_MOM_state, load_MOM_restart, init_MOM_ALE, destroy_MOM_state, do_regrid, domain_size
 
-  type, public :: MOM_state_type ; private
+  type, public :: MOM_state_type
     type(ocean_grid_type) :: G
     type(param_file_type) :: param_file
     type(hor_index_type) :: HI
@@ -30,19 +31,16 @@ module pyale_mod
     type(thermo_var_ptrs) :: tv
     type(regridding_CS) :: regrid_CS
 
-    real, dimension(:,:,:), allocatable :: h, T, S
+    real(real64), dimension(:,:,:), pointer :: h => NULL()
+    real(real64), dimension(:,:,:), pointer :: T => NULL()
+    real(real64), dimension(:,:,:), pointer :: S => NULL()
   end type MOM_state_type
 
 contains
 
-  subroutine init_MOM_state(ptr)
-    !f2py integer(8), intent(out) :: ptr
-    type(c_ptr), intent(out) :: ptr
-
-    type(MOM_state_type), pointer :: CS
+  subroutine init_MOM_state(CS)
+    type(MOM_state_type), intent(out) :: CS
     integer :: isd, ied, jsd, jed, nk
-    allocate(CS)
-    ptr = c_loc(CS)
 
     call open_param_file("MOM_input", CS%param_file)
     call MOM_domains_init(CS%G%domain, CS%param_file, symmetric=.true., domain_name="MOM_in")
@@ -56,42 +54,52 @@ contains
     call verticalGridInit(CS%param_file, CS%GV, CS%US)
 
     isd = CS%HI%isd ; ied = CS%HI%ied ; jsd = CS%HI%jsd ; jed = CS%HI%jed ; nk=CS%GV%ke
+    allocate(CS%h(isd:ied,jsd:jed,nk))
     allocate(CS%T(isd:ied,jsd:jed,nk))
     allocate(CS%S(isd:ied,jsd:jed,nk))
     CS%tv%T => CS%T ; CS%tv%S => CS%s
   end subroutine init_MOM_state
 
-  subroutine load_MOM_restart(ptr, restart_file)
-    !f2py integer(8), intent(in) :: ptr
-    type(c_ptr), intent(in) :: ptr
+  subroutine load_MOM_restart(CS, restart_file)
+    type(MOM_state_type), intent(inout) :: CS
     character(len=*), intent(in) :: restart_file
-
-    type(MOM_state_type), pointer :: CS
-    call c_f_pointer(ptr, CS)
 
     call MOM_read_data(trim(restart_file), "h", CS%h, CS%G%domain)
     call MOM_read_data(trim(restart_file), "Temp", CS%T, CS%G%domain)
     call MOM_read_data(trim(restart_file), "Salt", CS%S, CS%G%domain)
   end subroutine load_MOM_restart
 
-  subroutine init_MOM_ALE(ptr, regridding_scheme)
-    !f2py integer(8), intent(in) :: ptr
-    type(c_ptr), intent(in) :: ptr
+  subroutine init_MOM_ALE(CS, regridding_scheme)
+    type(MOM_state_type), intent(inout) :: CS
     character(len=*), intent(in) :: regridding_scheme
-
-    type(MOM_state_type), pointer :: CS
-    call c_f_pointer(ptr, CS)
 
     call initialize_regridding(CS%regrid_CS, CS%GV, CS%US, CS%G%max_depth, &
          CS%param_file, "init_MOM_state", trim(regridding_scheme), "", "")
   end subroutine init_MOM_ALE
 
-  subroutine destroy_MOM_state(ptr)
-    !f2py integer(8), intent(in) :: ptr
-    type(c_ptr), intent(in) :: ptr
+  subroutine domain_size(CS, dims)
+    type(MOM_state_type), intent(in) :: CS
+    integer, dimension(3), intent(out) :: dims
+    integer :: isd, ied, jsd, jed, nk
+    isd = CS%HI%isd ; ied = CS%HI%ied ; jsd = CS%HI%jsd ; jed = CS%HI%jed ; nk=CS%GV%ke
+    dims = [ied - isd + 1, jed - jsd + 1, nk]
+  end subroutine domain_size
 
-    type(MOM_state_type), pointer :: CS
-    call c_f_pointer(ptr, CS)
-    deallocate(CS)
+  subroutine do_regrid(CS, h_new)
+    type(MOM_state_type), intent(in) :: CS
+    real(real64), dimension(:,:,:), intent(out) :: h_new
+
+    integer :: isd, ied, jsd, jed, nk
+    real(real64), dimension(:,:,:), allocatable :: dz_regrid
+
+    isd = CS%HI%isd ; ied = CS%HI%ied ; jsd = CS%HI%jsd ; jed = CS%HI%jed ; nk=CS%GV%ke
+
+  end subroutine do_regrid
+
+  subroutine destroy_MOM_state(CS)
+    type(MOM_state_type), intent(inout) :: CS
+    if (associated(CS%h)) deallocate(CS%h)
+    if (associated(CS%T)) deallocate(CS%T)
+    if (associated(CS%S)) deallocate(CS%S)
   end subroutine destroy_MOM_state
 end module pyale_mod
