c5f2d974d86c8aae4fd06163a41569dade028bde
Angus Gibson <angus@agibson.me>
2022-06-27 10:43:13 +1000

    Build as an installable Python package
    Still need to make the interface nicer through the package, rather
    than the raw calls through f90wrap.

---
+++ setup.py
@@ -0,0 +1,59 @@
+import subprocess
+import sys
+import os
+
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=""):
+        super().__init__(name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+
+
+class CMakeBuild(build_ext):
+    def build_extension(self, ext):
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+
+        if not extdir.endswith(os.path.sep):
+            extdir += os.path.sep
+
+        debug = int(os.environ.get("DEBUG", 0)) if self.debug is None else self.debug
+        cfg = "Debug" if debug else "Release"
+
+        cmake_generator = os.environ.get("CMAKE_GENERATOR", "")
+
+        cmake_args = [
+            f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}",
+            f"-DCMAKE_BUILD_TYPE={cfg}",
+            "-DCMAKE_INSTALL_RPATH=$ORIGIN",
+        ]
+        build_args = []
+
+        if "CMAKE_ARGS" in os.environ:
+            cmake_args += [item for item in os.environ["CMAKE_ARGS"].split(" ") if item]
+
+        if not cmake_generator:
+            try:
+                import ninja
+                cmake_args += ["-GNinja"]
+            except ImportError:
+                pass
+
+        if "CMAKE_BUILD_PARALLEL_LEVEL" not in os.environ:
+            if hasattr(self, "parallel") and self.parallel:
+                build_args += [-f"-j{self.parallel}"]
+
+        build_temp = os.path.join(self.build_temp, ext.name)
+        if not os.path.exists(build_temp):
+            os.makedirs(build_temp)
+
+        subprocess.check_call(["cmake", ext.sourcedir] + cmake_args, cwd=build_temp)
+        subprocess.check_call(["cmake", "--build", "."] + build_args, cwd=build_temp)
+
+setup(
+    ext_modules=[CMakeExtension("pyale")],
+    packages=["pyale"],
+    cmdclass=dict(build_ext=CMakeBuild),
+    zip_safe=False,
+)
