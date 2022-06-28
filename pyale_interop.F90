937ce9167c80775d7a8aaaaf159ab36620b35ce6
Angus Gibson <angus@agibson.me>
2022-06-28 16:15:32 +1000

    Add binding for restart loading

--- pyale_interop.F90
+++ pyale_interop.F90
@@ -1,5 +1,5 @@
 subroutine init_MOM_state(CS, params) bind(C)
-  use iso_c_binding
+  use, intrinsic :: iso_c_binding
   use pyale_mod, only : MOM_state_type, f => init_MOM_state
   implicit none
 
@@ -12,8 +12,27 @@ subroutine init_MOM_state(CS, params) bind(C)
   CS = c_loc(fCS)
 end subroutine init_MOM_state
 
+subroutine load_MOM_restart(CS, cpath, pathlen) bind(C)
+  use, intrinsic :: iso_c_binding
+  use pyale_mod, only : MOM_state_type, f => load_MOM_restart
+  implicit none
+
+  type(c_ptr), intent(in), value :: CS
+  character(kind=c_char), dimension(*) :: cpath
+  integer(c_int), intent(in), value :: pathlen
+
+  character(len=pathlen) :: path
+  type(MOM_state_type), pointer :: fCS
+  integer :: i
+
+  forall (i = 1:pathlen) path(i:i) = cpath(i)
+  call c_f_pointer(CS, fCS)
+
+  call f(fCS, path)
+end subroutine load_MOM_restart
+
 subroutine destroy_MOM_state(CS) bind(C)
-  use iso_C_binding
+  use, intrinsic :: iso_c_binding
   use pyale_mod, only : MOM_state_type, f => destroy_MOM_state
   implicit none
 
--- pyalemodule.c
+++ pyalemodule.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 
 extern void init_mom_state(void*, PyObject*);
+extern void load_mom_restart(void*, char*, int);
 extern void destroy_mom_state(void*);
 
 static void pyale_destroy_cs(PyObject *capsule) {
@@ -17,8 +18,8 @@ static PyObject *pyale_init_cs(PyObject *self, PyObject *args) {
   PyObject *param_dict;
 
   // we expect a dict
-  ok = PyArg_ParseTuple(args, "O", &param_dict);
-  if (!ok || !PyDict_Check(param_dict))
+  ok = PyArg_ParseTuple(args, "O!", &PyDict_Type, &param_dict);
+  if (!ok)
     return NULL;
 
   init_mom_state(&cs, param_dict);
@@ -26,8 +27,27 @@ static PyObject *pyale_init_cs(PyObject *self, PyObject *args) {
   return cs_ptr;
 }
 
+static PyObject *pyale_load_restart(PyObject *self, PyObject *args) {
+  void *cs;
+  int ok;
+  PyObject *cs_ptr;
+  char *restart_buf;
+  Py_ssize_t buf_len;
+
+  ok = PyArg_ParseTuple(args, "O!s#", &PyCapsule_Type, &cs_ptr, &restart_buf, &buf_len);
+  if (!ok)
+    return NULL;
+
+  cs = PyCapsule_GetPointer(cs_ptr, "MOM6 CS");
+
+  load_mom_restart(cs, restart_buf, (int)buf_len);
+
+  Py_RETURN_NONE;
+}
+
 static PyMethodDef PyaleMethods[] = {
   {"mom_init_cs", pyale_init_cs, METH_VARARGS, "Initialise a MOM CS."},
+  {"load_mom_restart", pyale_load_restart, METH_VARARGS, "Load a MOM restart."},
   {NULL, NULL, 0, NULL},
 };
 
